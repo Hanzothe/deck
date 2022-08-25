a117082868fb31a680903ba19738b1ca6a10979f
Hanzothe <gui_oliveirasantos123@hotmail.com>
2022-08-25 03:33:03 -0300

    Done

--- deck/src/App.jsx
+++ deck/src/App.jsx
@@ -1,10 +1,8 @@
 import { useState } from "react";
 import "./index.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDotCircle } from "@fortawesome/free-regular-svg-icons";
 
 function App() {
-  const [minVal, setMinVal] = useState(0);
+  const [minVal, setMinVal] = useState(1);
   const [maxVal, setMaxVal] = useState(10);
   const [randomNum, setRandomNum] = useState(0);
 
@@ -15,10 +13,64 @@ function App() {
   return (
     <div className="App">
       <div id="container">
-        <button id="gera" onClick={handleRandomNum}>
-          <FontAwesomeIcon icon={faDotCircle} size="xs"></FontAwesomeIcon>
-        </button>
-        <p id="number">Numero Aleatório {randomNum}</p>
+        <div id="buttons">
+          <button
+            class="dices"
+            onMouseEnter={() => {
+              setMaxVal(4);
+            }}
+            onClick={() => {
+              handleRandomNum();
+            }}
+          >
+            D4
+          </button>
+          <button
+            class="dices"
+            onMouseEnter={() => {
+              setMaxVal(6);
+            }}
+            onClick={() => {
+              handleRandomNum();
+            }}
+          >
+            D6
+          </button>
+          <button
+            class="dices"
+            onMouseEnter={() => {
+              setMaxVal(8);
+            }}
+            onClick={() => {
+              handleRandomNum();
+            }}
+          >
+            D8
+          </button>
+          <button
+            class="dices"
+            onMouseEnter={() => {
+              setMaxVal(12);
+            }}
+            onClick={() => {
+              handleRandomNum();
+            }}
+          >
+            D12
+          </button>
+          <button
+            class="dices"
+            onMouseEnter={() => {
+              setMaxVal(20);
+            }}
+            onClick={() => {
+              handleRandomNum();
+            }}
+          >
+            D20
+          </button>
+        </div>
+        <div id="number">{randomNum}</div>
       </div>
     </div>
   );
