6a565edeed54a4ff362115ab6a1ccfa4a4a2acdd
Hanzothe <gui_oliveirasantos123@hotmail.com>
2022-10-18 15:28:07 -0300

    More and more css!

--- deck/src/Deck.jsx
+++ deck/src/Deck.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 
 function Deck() {
-  return (
-    <div className="Deck">
-      <div className="cube">
-        <div className="face"></div>
-      </div>
-    </div>
-  );
+  return <div className="card"></div>;
 }
 
 export default Deck;
